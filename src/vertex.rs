c9c7b11c9d0090e075b0cf0f68238b68a7744a56
Johnson, Ben <ben.johnson@hy-vee.com>
2022-06-10 13:15:14 -0500

    update vertices

--- src/vertex.rs
+++ src/vertex.rs
@@ -28,10 +28,10 @@ impl Vertex {
 }
 
 pub const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.0, 0.0] }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [1.0, 0.0] }, //B 
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.0, 1.0] }, // C
-    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [1.0, 1.0] }, // D
+    Vertex { position: [0.0, 0.5, 0.0], tex_coords: [0.0, 0.0] }, // A
+    Vertex { position: [-0.5, 0, 0.0], tex_coords: [1.0, 0.0] }, //B 
+    Vertex { position: [-0.25, -0.5, 0.0], tex_coords: [0.0, 1.0] }, // C
+    Vertex { position: [0.5, 0.25, 0.0], tex_coords: [1.0, 1.0] }, // D
 ];
 
 pub const INDICES: &[u16] = &[
