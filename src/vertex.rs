5f6740f5f23889444e87c1e2e6dadcb18021a2ed
Johnson, Ben <ben.johnson@hy-vee.com>
2022-06-10 13:28:25 -0500

    move vertex

--- src/vertex.rs
+++ src/vertex.rs
@@ -29,7 +29,7 @@ impl Vertex {
 
 pub const VERTICES: &[Vertex] = &[
     Vertex { position: [0.0, 0.5, 0.0], tex_coords: [0.0, 0.0] }, // A
-    Vertex { position: [-0.5, -0.5, 0.0], tex_coords: [1.0, 0.0] }, //B 
+    Vertex { position: [-0.5, 0.5, 0.0], tex_coords: [1.0, 0.0] }, //B 
     Vertex { position: [-0.25, -0.5, 0.0], tex_coords: [0.0, 1.0] }, // C
     Vertex { position: [0.5, 0.25, 0.0], tex_coords: [1.0, 1.0] }, // D
 ];
