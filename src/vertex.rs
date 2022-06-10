8d4a006334e6f18a2d96851c9e0ff6df43809150
Johnson, Ben <ben.johnson@hy-vee.com>
2022-06-10 12:54:06 -0500

    add texture

--- src/vertex.rs
+++ src/vertex.rs
@@ -3,7 +3,7 @@
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct Vertex {
     position: [f32; 3],
-    color: [f32; 3]
+    tex_coords: [f32; 2]
 }
 
 impl Vertex {
@@ -20,7 +20,7 @@ impl Vertex {
                 wgpu::VertexAttribute {
                     offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
                     shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3
+                    format: wgpu::VertexFormat::Float32x2
                 }
             ]
         }
@@ -28,15 +28,13 @@ impl Vertex {
 }
 
 pub const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0]},
-    Vertex { position: [-0.475528258148, 0.154508497187, 0.0], color: [0.0, 1.0, 0.0]},
-    Vertex { position: [-0.293892626146, -0.404508497187, 0.0], color: [0.0, 0.0, 1.0]},
-    Vertex { position: [0.293892626146, -0.404508497187, 0.0], color: [1.0, 1.0, 0.0]},
-    Vertex { position: [0.475528258148, 0.154508497187, 0.0], color: [0.0, 1.0, 1.0]},
+    Vertex { position: [-0.5, -0.5, 0.0], tex_coords: [0.0, 0.0]}, //Top left
+    Vertex { position: [ 0.5, -0.5, 0.0], tex_coords: [1.0, 0.0]}, //Top right
+    Vertex { position: [-0.5,  0.5, 0.0], tex_coords: [0.0, 1.0]}, //Bottom left
+    Vertex { position: [ 0.5,  0.5, 0.0], tex_coords: [1.0, 1.0]}, //Bottom right
 ];
 
 pub const INDICES: &[u16] = &[
-    0, 1, 4,
-    1, 2, 4,
-    2, 3, 4,
+    0, 3, 1,
+    0, 2, 3
 ];
